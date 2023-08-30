5a20d38fde5356df498c3ee467933eef8922a778
kleber chavez <kleberenriqueos@gmail.com>
2023-08-30 15:48:38 -0500

    clase factura

--- ModuloFarmacia/src/main/java/Modelo/Factura.java
+++ ModuloFarmacia/src/main/java/Modelo/Factura.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Modelo;
 
-/**
- *
- * @author HP
- */
 public class Factura {
+    private String nombre,codigo;
+    private float precio;
+
+    public Factura(String nombre, String codigo, float precio) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.precio = precio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
     
 }
