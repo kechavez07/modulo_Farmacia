2d71194e6fb3e79d2be93539bc3dd8b58b805466
victoria diaz <cvdiaz3@espe.edu.ec>
2023-08-30 15:26:50 -0500

    mi primer commit

---
+++ ModuloFarmacia/src/main/java/Modelo/Medicamento.java
@@ -0,0 +1,59 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author HP
+ */
+public class Medicamento {
+   private String nombre,codigo;
+   private float precio;
+   private int stock;
+
+    public Medicamento(String nombre, String codigo, float precio, int stock) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+   
+   public void registrarMedicamento(){
+       System.out.println("ayudaaaaaaa ya no sé que hacer...");
+   }
+  
+}
---
+++ ModuloFarmacia/src/main/java/com/mycompany/modulofarmacia/ModuloFarmacia.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.modulofarmacia;
+
+import Vista.Principal;
+
+/**
+ *
+ * @author HP
+ */
+public class ModuloFarmacia {
+
+    public static void main(String[] args) {
+       Principal principal= new Principal();
+       principal.show(true);
+       
+    }
+}
